4aefbdc1a2985d08ba689db99294996f4b5ea130
Matthew Reda <m.reda94@gmail.com>
2022-11-03 16:03:26 -0400

    Add `say_hello`; update index

--- README.md
+++ README.md
@@ -17,7 +17,7 @@ pip install nbdev_test
 Fill me in please! Don’t forget code examples:
 
 ``` python
-1+1
+say_hello("Matthew")
 ```
 
-    2
+    'Hello Matthew!'
--- nbdev_test/core.py
+++ nbdev_test/core.py
@@ -3,7 +3,7 @@
 # %% auto 0
 __all__ = ['say_hello', 'foo']
 
-# %% ../nbs/00_core.ipynb 2
+# %% ../nbs/00_core.ipynb 3
 def say_hello(to):
     "Say hello to somebody"
     return f'Hello {to}!'
